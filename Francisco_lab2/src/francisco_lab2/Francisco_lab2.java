71d274d946d64ff65e9c6dc7381c153507b8282f
Francisco Santos <fjgsantos98@gmail.com>
2017-07-28 16:18:43 -0600

    Hice el eliminar

--- Francisco_lab2/src/francisco_lab2/Francisco_lab2.java
+++ Francisco_lab2/src/francisco_lab2/Francisco_lab2.java
@@ -8,8 +8,8 @@ public class Francisco_lab2 {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        ArrayList<Usuario> users=new ArrayList();
-        Usuario p=new Usuario();
+        ArrayList<Usuario> users = new ArrayList();
+        Usuario p = new Usuario();
         String r = "s";
         while ("S".equals(r) || "s".equals(r)) {
             int opc = Integer.parseInt(JOptionPane.showInputDialog(("|||||| Red Social ||||||\n"
@@ -18,31 +18,62 @@ public class Francisco_lab2 {
                     + "3. Eliminar Usuario\n"
                     + "4. Log In\n"
                     + "Ingrese la Opcion a desarrollar")));
-            
+
             switch (opc) {
                 case 1:
                     String nombre = JOptionPane.showInputDialog("Ingrese el Nombre:");
                     int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Su Edad:"));
                     String lugar = JOptionPane.showInputDialog("Ingrese Lugar de Nacimiento:");
-                    String username= JOptionPane.showInputDialog("Ingrese el Username:");
-                    String password= JOptionPane.showInputDialog("Ingrese la Contraseña:");
-                    for (Usuario object :users ) {
-                        while (username.equals(object.getusername())){
-                            username= JOptionPane.showInputDialog("El usuario ya existe ingrese otro:");
+                    String username = JOptionPane.showInputDialog("Ingrese el Username:");
+                    String password = JOptionPane.showInputDialog("Ingrese la Contraseña:");
+                    for (Usuario object : users) {
+                        while (username.equals(object.getusername())) {
+                            username = JOptionPane.showInputDialog("El usuario ya existe ingrese otro:");
                         }
-                        users.add(new Usuario(nombre,edad,lugar,username,password));
+                        users.add(new Usuario(nombre, edad, lugar, username, password));
                     }
                     break;
                 case 2:
-                    int Num_User=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de Usuario que desea modificar:"));
-                    
-                    
+                    String nuevo="";
+                    for (Object t : users) {
+                        if (t instanceof Usuario) {
+                            nuevo +=(users.indexOf(t)+1)+"-."+((Usuario) t).getnombre()+"\n";
+                        }
+                    }
+                    int user= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero del usuario que desea modificar"));
+                    int Num_User = Integer.parseInt(JOptionPane.showInputDialog(
+                              "1. Nombre"
+                            + "2. Edad"
+                            + "3. Lugar de Nacimiento"
+                            + "4. Username"
+                            + "5. Password"
+                            + "Ingrese la Opcion que desea Modificar "));
+                    switch (Num_User) {
+                        case 1:
+                            
+                            
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+
+                            break;
+                        case 5:
+
+                            break;
+                    }
+
                     break;
                 case 3:
-                    int Num2_User=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de Usuario que desea eliminar:"));
+                    int Num2_User = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de Usuario que desea eliminar:"));
+
                     break;
                 case 4:
-                    
+
                     break;
             }
         }
